5d138a1774800940b7cbf691d6687714bf52a367
enrique-22 <00055014@uca.edu.sv>
2021-03-14 18:48:47 -0600

    Subiendo el primer archivo

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Firstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int a = 0, b = 0;
+
+          Console.WriteLine("Enter a value for a:");
+
+          a = Int32.Parse(Console.ReadLine());
+
+          Console.WriteLine("Enter a value for  b:");
+          
+          b= Int32.Parse(Console.ReadLine());
+
+          Console.WriteLine($"The sum is {a} + {b} = {AddNumbers(a,b)}");
+          //Console.WriteLine($"The subtraction is {a} - {b} = {SubtractingaNumber(a,b)}");
+
+        }
+
+         public static int AddNumbers(int a , int b){
+
+             return a + b;
+
+             
+         }
+        //public static int SubtractingaNumber (int a, int b){
+
+            //return a - b;
+        //}
+
+    }
+   
+        
+}
