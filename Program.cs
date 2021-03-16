d93ea7042f12f8dc0fbe26f2f23b5d7cdf445604
enrique-22 <00055014@uca.edu.sv>
2021-03-16 14:32:47 -0600

    realiza multiplicacion

--- Program.cs
+++ Program.cs
@@ -15,24 +15,27 @@ namespace Firstprogram
           Console.WriteLine("Enter a value for  b:");
           
           b= Int32.Parse(Console.ReadLine());
-
-          Console.WriteLine($"The sum is {a} + {b} = {AddNumbers(a,b)}");
+          
+          Console.WriteLine($"the multiplication is {a} * {b} = {MultiplyaNumber(a,b)}");
+          //Console.WriteLine($"The sum is {a} + {b} = {AddNumbers(a,b)}");
           //Console.WriteLine($"The subtraction is {a} - {b} = {SubtractingaNumber(a,b)}");
-
+           
         }
 
-         public static int AddNumbers(int a , int b){
+        /*public static int AddNumbers(int a , int b)
+        {
+            return a + b;
+        }
 
-             return a + b;
+        public static int SubtractingaNumber(int a, int b)
+        {
+           return a - b;
+        }*/
 
-             
-         }
-        //public static int SubtractingaNumber (int a, int b){
-
-            //return a - b;
-        //}
-
+        public static int MultiplyaNumber(int a, int b)
+       {
+           return a*b;
+       }
     }
-   
-        
+       
 }
